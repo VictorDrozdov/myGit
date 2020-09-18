71ec7bd2a98ed28155b5c201b011d089d80e5837
Victor Drozdov <victoraleksandrovich4@gmail.com>
2020-09-18 15:04:30 +0300

    release v1

---
+++ Laba_1/src/main/java/org/drozdov/Laba_1/HelloController.java
@@ -0,0 +1,13 @@
+package org.drozdov.Laba_1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String home(){
+
+        return "home";
+    }
+}
